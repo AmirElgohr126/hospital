7a94823258edbda15b6525a86d38802027a705e7
Amir nagy <amirnagy886@gmail.com>
2025-06-04 03:20:42 +0300

    finish them of auth [WIP] user role

--- config/auth.php
+++ config/auth.php
@@ -60,11 +60,16 @@ return [
     */
 
     'providers' => [
+
         'users' => [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
         ],
 
+        'admin' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Admin::class,
+        ],
         // 'users' => [
         //     'driver' => 'database',
         //     'table' => 'users',
@@ -93,6 +98,13 @@ return [
     'passwords' => [
         'users' => [
             'provider' => 'users',
+            'table' => 'password_reset_tokens',
+            'expire' => 60,
+            'throttle' => 60,
+        ],
+
+        'admin' => [
+            'provider' => 'admin',
             'table' => 'password_reset_tokens',
             'expire' => 60,
             'throttle' => 60,
--- routes/admin_panel.php
+++ routes/admin_panel.php
@@ -1,10 +1,34 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Dashboard\DashboardController;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 
 
-Route::group(['prefix'=>'admin-panel'], function (Router $router) {
-    $router->get('/', [DashboardController::class,'index']);
+Route::group(['middleware'=>'guest'], function (Router $router) {
+    $router->get('register', [RegisteredUserController::class, 'create'])->name('register'); // done
+
+    $router->post('register', [RegisteredUserController::class, 'store'])->name('register.store'); // done
+
+    $router->get('login', [AuthenticatedSessionController::class, 'create']); // done
+
+    $router->post('login', [AuthenticatedSessionController::class, 'store'])->name('login'); // done
+
+    $router->get('forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request'); // done
+
+    $router->post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
+
+    $router->get('reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
+
+    $router->post('reset-password', [NewPasswordController::class, 'store'])->name('password.store');
+});
+
+Route::group(['middleware' => 'auth'], function (Router $router) {
+    $router->get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    $router->post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 });
